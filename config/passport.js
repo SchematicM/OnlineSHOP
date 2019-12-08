c2d6702aa02297adb7ea2f30b0ae1724639a9772
Martina Skrypnyk <37904208+SchematicM@users.noreply.github.com>
2019-12-09 02:52:16 +0300

    Added Validation onEmail(isEmail()) & Password(min count of character = 4)

--- config/passport.js
+++ config/passport.js
@@ -18,7 +18,19 @@ usernameField: 'email',
 passportField: 'password' ,
 passReqToCallback: true
 }, function(req, email, password, done) {
-User.findOne({'email': email}, function(err, user)
+	req.checkBody('email', 'Invalid e-mail').notEmpty().isEmail();
+	req.checkBody('password', 'Invalid password').notEmpty().isLength({min:4});
+	var errors =req.validationErrors();
+	if(errors)
+	{
+		var messages = [];
+		errors.forEach(function(error){
+			messages.push(error.msg);
+		});
+		return done(null,false,req.flash('error',messages));
+	}
+
+    User.findOne({'email': email}, function(err, user)
 {
 	if (err) {
 		return done(err);
