1c6c18103f1a28fdee1763715b851a926e4289d2
SolomiyaRybai <Solomiyar2000@gmail.com>
2019-12-08 18:32:59 +0200

    user

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var router = express.Router();
 var Product = require ('../models/product');
+var csrf = require('csurf');
+
+var csrfProtection = csrf(); 
+router.use(csrfProtection);
 /* GET home page. */
 router.get('/', function(req, res, next) 
 {
 	Product.find(function(err, docs)
 		{
-			console.log(docs);
 			var productChunks = [];
 			var chunkSize = 3;
 			for (var i = 0; i < docs.length; i += chunkSize)
@@ -17,4 +20,13 @@ router.get('/', function(req, res, next)
 		});
 });
 
+router.get('/user/signup', function(req, res, next)
+{
+	res.render('user/signup', {csrfToken: req.csrfToken()});
+});
+
+router.post('/user/signup', function(req, res, next)
+{
+	res.redirect('/');
+});
 module.exports = router;
